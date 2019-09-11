3fe0991343324cbd4704188415e08565cf7cea17
stigsanek <stigsanek@yandex.ru>
2019-09-11 15:04:52 +0400

    Добавляет моки

---
+++ js/data.js
@@ -0,0 +1,97 @@
+'use strict';
+
+(function () {
+  window.data = {
+    workers : [
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Дейенерис Таргариен',
+        departament: 'Руководство',
+        position: 'Директор',
+        rang: 1,
+        phone: '100'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Джон Сноу',
+        departament: 'Руководство',
+        position: 'Заместитель директора',
+        rang: 2,
+        phone: '101'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Тирион Ланнистер',
+        departament: 'Руководство',
+        position: 'Секретарь руководителя',
+        rang: 3,
+        phone: '103',
+        secretary: 'Приемная'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Санса Старк',
+        departament: 'Отдел продаж',
+        position: 'Начальник отдела',
+        rang: 4,
+        phone: '104'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Брандон Старк',
+        departament: 'Отдел продаж',
+        position: 'Менеджер по продажам',
+        rang: 6,
+        phone: '105'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Джейме Ланнистер',
+        departament: 'Отдел разработки',
+        position: 'Начальник отдела',
+        rang: 4,
+        phone: '106'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Арья Старк',
+        departament: 'Отдел разработки',
+        position: 'Разработчик',
+        rang: 6,
+        phone: '107'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Сандор Клиган',
+        departament: 'Отдел кадров',
+        position: 'Заместитель начальника отдела',
+        rang: 5,
+        phone: '108'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Григор Клиган',
+        departament: 'Отдел кадров',
+        position: 'HR-менеджер',
+        rang: 6,
+        phone: '109'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Серсея Ланнистер',
+        departament: 'Отдел аналитики',
+        position: 'Начальник отдела',
+        rang: 4,
+        phone: '110'
+      },
+      {
+        photo: 'img/photos/default_avatar.png',
+        name: 'Джоффри Баратеон',
+        departament: 'Отдел аналитики',
+        position: 'Главный аналитик',
+        rang: 6,
+        phone: '111'
+      }
+    ]
+  };
+})();
